17eba6487758f13af818df6bcfba4afc712d566d
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-01-01 17:57:23 +0000

    Added copyright, GNU LGPL license

--- media_core/include/media_debug.h
+++ media_core/include/media_debug.h
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (C) 2011 Prem Sasidharan.
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public License 
+ * published by the Free Software Foundation.
+*/
+
 #ifndef _MEDIA_DEBUG_H
 #define _MEDIA_DEBUG_H
 
@@ -5,29 +13,6 @@
 
 #define MEDIA_ERROR(fmt, ...) Media_logger::instance().log(Media_logger::console, fmt, __VA_ARGS__)
 #define MEDIA_WARNING(fmt, ...) Media_logger::instance().log(Media_logger::console, fmt, __VA_ARGS__)
-
-#if 0
-#include <stdio.h>
-#include <typeinfo>
-
-#define OBJ_ERROR(fmt, ...) fprintf(stderr, "\n\tError: %s::%s() " fmt, typeid(this).name(), __func__, __VA_ARGS__)
-#define OBJ_WARNING(fmt, ...) fprintf(stderr, "\n\tWarning: %s::%s() " fmt, typeid(this).name(), __func__, __VA_ARGS__)
-#define _ERROR(fmt, ...) fprintf(stderr, "\n\t%s() " fmt, __func__, __VA_ARGS__)
-
-#ifdef DEBUG_TRACE
-#define TRACE(fmt, ...) fprintf(stderr, "%s() in %s, line %i: " fmt "\n", __func__, __FILE__, __LINE__, __VA_ARGS__)
-#define OBJ_TRACE(fmt, ...) fprintf(stderr, "%s::%s() line #: %i " fmt "\n", typeid(this).name(), __func__, __LINE__, __VA_ARGS__)
-#else
-#define TRACE(fmt, ...) ((void)0)
-#define OBJ_TRACE(fmt, ...) ((void)0)
-#endif
-
-#ifdef OBJECT_DEBUG
-#define OBJ_DBG(fmt, ...) fprintf(stderr, "%s() in %s, line %i: " fmt "\n", __func__, __FILE__, __LINE__, __VA_ARGS__)
-#else
-#define OBJ_DBG(fmt, ...) ((void)0)
-#endif
-#endif
 
 #endif
 
