555e7234476ec521c10c7c8eafd5d0123ffc9158
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-01-01 18:07:18 +0000

    Added copyright, GNU LGPL license

--- trunk/audio_renderer/include/audio_renderer.h
+++ trunk/audio_renderer/include/audio_renderer.h
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (C) 2011 Prem Sasidharan.
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public License 
+ * published by the Free Software Foundation.
+*/
+
 #ifndef _AUDIO_RENDERER_H
 #define _AUDIO_RENDERER_H
 
