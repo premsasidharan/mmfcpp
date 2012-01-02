edf1db7662e70c805791b73ee4361017c0e9ec70
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-01-02 06:25:16 +0000

    Added qt_renderer test program

---
+++ player/source/qt_test.cpp
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (C) 2011 Prem Sasidharan.
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public License 
+ * published by the Free Software Foundation.
+*/
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+
+#include <yuv_window.h>
+#include <xv_renderer.h>
+#include <av_file_src.h>
+#include <audio_renderer.h>
+#include <avcodec_audio_decoder.h>
+#include <avcodec_video_decoder.h>
+
+#include <QtGui/QApplication>
+
+int main(int argc, char** argv)
+{
+    MEDIA_TRACE();
+    
+    QApplication app(argc, argv);
+    Yuv_window window(0, 0, 320, 240);	
+    
+	int time = 0;
+	Av_file_src src("av");
+	Xv_renderer video_sink("xv", &window);
+	Audio_renderer audio_sink("alsa", "default");
+    Avcodec_audio_decoder audio_decoder("audio_decoder");
+    Avcodec_video_decoder video_decoder("video_decoder");
+
+	if (argc < 2)
+	{
+		printf("\n\tInsufficien arguments\n");
+		exit(0);
+	}
+
+	if (1 == src.set_file_path(argv[1]))
+	{
+        window.show();
+		connect(src, video_decoder);
+		connect(src, audio_decoder);
+		connect(audio_decoder, audio_sink);
+		connect(video_decoder, video_sink);
+		start(src, 0);
+		app.exec();
+		stop(src, time);
+		disconnect(audio_decoder, audio_sink);
+		disconnect(video_decoder, video_sink);
+		disconnect(src, audio_decoder);
+		disconnect(src, video_decoder);
+	}
+	else
+	{
+		printf("\nInvalid File Path");
+	}
+	return 0;
+}
+
