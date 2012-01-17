bc555eb4801119678d09e8a74181dc7114b4ac2c
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-01-17 07:23:26 +0000

    adjusted formatting

--- trunk/qt_player/include/player.h
+++ trunk/qt_player/include/player.h
@@ -1,8 +1,8 @@
 /*
  *  Copyright (C) 2011 Prem Sasidharan.
  *
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
  * published by the Free Software Foundation.
 */
 #ifndef _PLAYER_H
@@ -19,26 +19,25 @@
 
 class Player:public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	Player(const QString path);
-	~Player();
+    Player(const QString path);
+    ~Player();
 
 public:
-	int start();
-	int stop();
+    int start();
+    int stop();
 
 protected slots:
-	void on_renderer_close();
+    void on_renderer_close();
 
 private:
-	Yuv_window window;
-	Av_file_src av_src;
-	Xv_renderer video_sink;
-	Audio_renderer audio_sink;
+    Yuv_window window;
+    Av_file_src av_src;
+    Xv_renderer video_sink;
+    Audio_renderer audio_sink;
     Avcodec_audio_decoder audio_decoder;
     Avcodec_video_decoder video_decoder;
 };
 
 #endif
- 
--- trunk/qt_player/source/player.cpp
+++ trunk/qt_player/source/player.cpp
@@ -1,55 +1,54 @@
 /*
  *  Copyright (C) 2011 Prem Sasidharan.
  *
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
  * published by the Free Software Foundation.
 */
 #include <player.h>
 
 Player::Player(const QString path)
-	:QObject()
-	, window(0, 0, 320, 240)
-	, av_src("av")
-	, video_sink("xv", &window)
-	, audio_sink("alsa", "default")
+    :QObject()
+    , window(0, 0, 320, 240)
+    , av_src("av")
+    , video_sink("xv", &window)
+    , audio_sink("alsa", "default")
     , audio_decoder("audio_decoder")
     , video_decoder("video_decoder")
 {
-	if (1 == av_src.set_file_path(path.toAscii().constData()))
-	{
-		connect(&window, SIGNAL(renderer_close()), this, SLOT(on_renderer_close()));
+    if (1 == av_src.set_file_path(path.toAscii().constData()))
+    {
+        connect(&window, SIGNAL(renderer_close()), this, SLOT(on_renderer_close()));
 
         window.show();
-		::connect(av_src, video_decoder);
-		::connect(av_src, audio_decoder);
-		::connect(audio_decoder, audio_sink);
-		::connect(video_decoder, video_sink);	
-	}
+        ::connect(av_src, video_decoder);
+        ::connect(av_src, audio_decoder);
+        ::connect(audio_decoder, audio_sink);
+        ::connect(video_decoder, video_sink);
+    }
 }
 
 Player::~Player()
 {
-	::disconnect(audio_decoder, audio_sink);
-	::disconnect(video_decoder, video_sink);
-	::disconnect(av_src, audio_decoder);
-	::disconnect(av_src, video_decoder);
+    ::disconnect(audio_decoder, audio_sink);
+    ::disconnect(video_decoder, video_sink);
+    ::disconnect(av_src, audio_decoder);
+    ::disconnect(av_src, video_decoder);
 }
 
 int Player::start()
 {
-	return ::start(av_src, 0);
+    return ::start(av_src, 0);
 }
 
 int Player::stop()
 {
-	int time;
-	return ::stop(av_src, time);
+    int time;
+    return ::stop(av_src, time);
 }
 
 void Player::on_renderer_close()
 {
-	int time;
-	::stop(av_src, time);
+    int time;
+    ::stop(av_src, time);
 }
-
