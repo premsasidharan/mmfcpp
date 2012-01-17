03aa54f7d2c48e3e70f85604f4ea4f01f9e1d587
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-01-17 07:23:26 +0000

    adjusted formatting

--- qt_player/source/main.cpp
+++ qt_player/source/main.cpp
@@ -1,8 +1,8 @@
 /*
  *  Copyright (C) 2011 Prem Sasidharan.
  *
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
  * published by the Free Software Foundation.
 */
 #include <QString>
@@ -12,17 +12,16 @@
 
 int main(int argc, char** argv)
 {
-    MEDIA_TRACE();   
+    MEDIA_TRACE();
     QApplication app(argc, argv);
 
-	if (argc < 2)
-	{
-		printf("\n\tInsufficien arguments\n");
-		exit(0);
-	}
-	const QString str(argv[1]);
-	Player player(str);
-	player.start();
-	return app.exec();
+    if (argc < 2)
+    {
+        printf("\n\tInsufficien arguments\n");
+        exit(0);
+    }
+    const QString str(argv[1]);
+    Player player(str);
+    player.start();
+    return app.exec();
 }
-
