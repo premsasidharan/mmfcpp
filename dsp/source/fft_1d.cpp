d8bdb49ec7da5ca2fab66e8e1e128e753a9e5cdd
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-01-31 07:31:28 +0000

    Corrected formatting

--- dsp/source/fft_1d.cpp
+++ dsp/source/fft_1d.cpp
@@ -14,24 +14,24 @@ int compute_fft_1d(complex* data, int length)
     double angle;
     complex wstart, wdata;
     int steps, stage_size = 1;
-	for (int i = 0; i < stage_count; i++)
-	{
+    for (int i = 0; i < stage_count; i++)
+    {
         steps = (stage_size<<1);
-		angle = (-1*M_PI)/((double)stage_size);
-		wstart = complex::polar(1.0, angle);
+        angle = (-1*M_PI)/((double)stage_size);
+        wstart = complex::polar(1.0, angle);
         wdata = complex::cartesian(1.0, 0.0);
-		for (int j = 0; j < stage_size; j++)
-		{
+        for (int j = 0; j < stage_size; j++)
+        {
             wdata *= wstart;
-			for (int top = j; top < length; top += steps)
-			{
-				int bottom = top+stage_size;
-				complex temp = data[bottom]*wdata;
-			 	data[bottom] = data[top]-temp;
-				data[top] += temp;
-			}
-		}
-		stage_size = steps;
-	}
+            for (int top = j; top < length; top += steps)
+            {
+                int bottom = top+stage_size;
+                complex temp = data[bottom]*wdata;
+                data[bottom] = data[top]-temp;
+                data[top] += temp;
+            }
+        }
+        stage_size = steps;
+    }
     return 1;
 }
