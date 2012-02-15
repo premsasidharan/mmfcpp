b5b12f9f365614b353ae55acadd5aea3baaf8645
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-02-15 04:28:24 +0000

    Added audio_test, used class Read_wave_file

---
+++ test/audio_test/source/audio_test.cpp
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (C) 2011 Prem Sasidharan.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * published by the Free Software Foundation.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <wave_file_src.h>
+#include <audio_renderer.h>
+
+int main(int argc, char** argv)
+{
+    int time = 0;
+    MEDIA_TRACE();
+    Wave_file_src src("wave");
+    Audio_renderer sink("alsa", "default");
+
+    if (argc < 2)
+    {
+        printf("\n\tInsufficien arguments\n");
+        exit(0);
+    }
+
+    if (1 == src.set_file_path(argv[1]))
+    {
+        connect(&src, &sink);
+        start(&src, 0);
+        sleep(10);
+        stop(&src, time);
+        disconnect(&src, &sink);
+    }
+    else
+    {
+        printf("\nInvalid Wave File Path");
+    }
+    return 0;
+}
--- test/wave_file_test/source/wave_file_test.cpp
+++ test/wave_file_test/source/wave_file_test.cpp
@@ -1,9 +1,12 @@
 #include <string.h>
 #include <stdlib.h>
+#include <alsa/asoundlib.h>
+
 #include <read_wave_file.h>
 #include <write_wave_file.h>
 
 void usage();
+void play_test(char* path);
 void read_test(char* path);
 void write_test(char* path);
 
@@ -22,11 +25,42 @@ int main(int argc, char** argv)
     {
         write_test(argv[2]);
     }
+    else if (0 == strcmp(argv[1], "-p"))
+    {
+        play_test(argv[2]);
+    }
     else
     {
         usage();
     }
     return 0;
+}
+
+snd_pcm_format_t format(unsigned int bps)
+{
+    snd_pcm_format_t fmt = SND_PCM_FORMAT_U8;
+
+    switch (bps)
+    {
+        case 8:
+            fmt = SND_PCM_FORMAT_U8;
+            break;
+        case 16:
+            fmt = SND_PCM_FORMAT_S16_LE;
+            break;
+        case 24:
+            fmt = SND_PCM_FORMAT_S24_LE;
+            break;
+        case 32:
+            fmt = SND_PCM_FORMAT_S32_LE;
+            break;
+    }
+    return fmt;
+}
+
+void usage()
+{
+    printf("\n\nUsage...\nwave_file_test -[r/w] file_path\n\n\n");
 }
 
 void read_test(char* path)
@@ -70,7 +104,52 @@ void write_test(char* path)
     }
 }
 
-void usage()
+void play_test(char* path)
 {
-    printf("\n\nUsage...\nwave_file_test -[r/w] file_path\n\n\n");
+    Read_wave_file file;
+    if (file.open(path))
+    {
+		struct timespec time;
+    	snd_pcm_t* pcm_handle = 0;
+		unsigned long available = 0;
+		unsigned long buffer_size = 0, period_size = 0;
+		int error = snd_pcm_open(&pcm_handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
+		error = (error < 0)?error:snd_pcm_set_params(pcm_handle, 
+									format(file.bits_per_sample()), 
+									SND_PCM_ACCESS_RW_INTERLEAVED, 
+									file.channel_count(), 
+									file.sample_rate(), 0, 0);
+		error = (error <0)?error:snd_pcm_get_params(pcm_handle, 
+							&buffer_size, &period_size);
+		error = (error < 0)?error:snd_pcm_prepare(pcm_handle);
+		error = (error < 0)?error:snd_pcm_reset(pcm_handle);
+		printf("\n\tBuffer Size: %ld, Period Size: %ld", buffer_size, period_size);
+		if (error >= 0)
+		{
+        	int size = file.frame_size()*1024;
+        	unsigned char* buffer = new unsigned char[size];
+        	int count = 0, size_read = 0;
+        	while (0 == file.is_eof())
+        	{
+            	++count;
+            	file.read(buffer, size, size_read);
+				snd_pcm_htimestamp(pcm_handle, &available, &time);
+            	printf("\n\tcount: %d, size: %d, size_read: %d, avail. frames: %ld, time: %d:%d", count, size, size_read, available, time.tv_sec, time.tv_nsec);
+				error = snd_pcm_writei(pcm_handle, buffer, 1024);
+				snd_pcm_htimestamp(pcm_handle, &available, &time);
+				error = (error >= 0)?error:snd_pcm_recover(pcm_handle, error, 0);
+            	printf("\n\tcount: %d, size: %d, size_read: %d, avail. frames: %ld, time: %d:%d", count, size, size_read, available, time.tv_sec, time.tv_nsec);
+        	}
+        	delete [] buffer;
+        	buffer = 0;
+		}
+		snd_pcm_close(pcm_handle);
+		pcm_handle = 0;
+        printf("\nSample rate: %d", file.sample_rate());
+        printf("\nChannel count: %d", file.channel_count());
+        printf("\nBits per sample: %d", file.bits_per_sample());
+        printf("\nTotal frames: %d", file.frames_count());
+        file.close();
+    }
 }
+
