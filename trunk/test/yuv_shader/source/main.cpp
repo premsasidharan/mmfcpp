831a3ec3ae978d05d2abd5274fc87c36508b3e4d
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-03-27 14:11:52 +0000

    added commandline parameters

--- trunk/test/yuv_shader/source/main.cpp
+++ trunk/test/yuv_shader/source/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
-	Yuv_window window;
+	Yuv_window window(atoi(argv[2]), atoi(argv[3]), QString(argv[1]));
 	window.resize(600, 600);
 	window.show();
 	return app.exec();
