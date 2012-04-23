ddc32743b8aed6f25d6a2d10a4d9925e015ddc08
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-04-23 14:22:13 +0000

    added yuv video player

---
+++ trunk/test/video_player/include/video_player.h
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (C) 2011 Prem Sasidharan.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * published by the Free Software Foundation.
+*/
+
+#ifndef _VIDEO_PLAYER_H_
+#define _VIDEO_PLAYER_H_
+
+#include <QTimer>
+#include <QSlider>
+#include <QHBoxLayout>
+#include <QPushButton>
+
+#include <video_widget.h>
+#include <yuv_file_src.h>
+#include <video_renderer.h>
+
+class Video_player:public QWidget, public Observer
+{
+    Q_OBJECT
+public:
+    Video_player();
+    ~Video_player();
+    
+public:
+    void show();
+    
+    int start(int start, int end);
+    int stop(int& time);
+    int duration() const;
+    int set_parameters(int width, int height, Media::type fmt, float fps, const char* path);
+    
+protected:
+    void initialize();
+    int event_handler(Media::events event, Abstract_media_object* obj, Media_params& params);
+
+protected slots:
+    void on_timeout();
+    void on_play_pause();
+    void slider_pressed();
+    void slider_released();
+
+private:
+    QTimer timer;
+    QSlider slider;
+    int trick_mode;
+    QPushButton button;
+    QHBoxLayout layout;
+    
+    Video_widget window;
+    Yuv_file_src source;
+    Video_renderer sink;
+};
+
+#endif
---
+++ trunk/test/video_player/video_player.pro
@@ -0,0 +1,36 @@
+TEMPLATE = app
+TARGET = video_player
+DESTDIR = bin
+MOC_DIR = obj
+OBJECTS_DIR = obj
+QT += opengl
+DEPENDPATH += .
+INCLUDEPATH += ../../media_core/include \
+			   ../../video_widget/include \
+			   ../../yuv_file/include \
+			   ../../yuv_file_src/include \
+			   ../../video_renderer/include \
+			   ./include \
+			   ./obj \
+
+LIBS += -L/usr/lib -lpthread -lm -lsupc++ -lrt -L../../media_core/lib -lmedia_core
+
+HEADERS = include/video_player.h \
+          ../../video_widget/include/video_widget.h
+
+SOURCES = ../../video_widget/source/video_widget.cpp \
+	      ../../yuv_file/source/yuv_file.cpp \
+	      ../../yuv_file/source/read_yuv_file.cpp \
+	      ../../yuv_file_src/source/yuv_file_src.cpp \
+		  ../../video_renderer/source/video_renderer.cpp \
+          source/video_player.cpp \
+          source/main.cpp
+
+CONFIG += qt debug
+
+media_core.commands = make -C ../../media_core -f libmedia_core.mk
+QMAKE_EXTRA_TARGETS += media_core
+PRE_TARGETDEPS += media_core
+
+QMAKE_CLEAN += -r ../../media_core/lib -r ../../media_core/obj
+
