c78d7070ab85594b778350cf9ecb3fcbbf550734
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-08-31 06:52:37 +0000

    Added copyright

--- apps/yuv_player/include/yuv_dlg.h
+++ apps/yuv_player/include/yuv_dlg.h
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (C) 2011 Prem Sasidharan.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * published by the Free Software Foundation.
+*/
+
 #ifndef _YUV_DLG_H_
 #define _YUV_DLG_H_
 
