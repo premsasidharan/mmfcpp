1364e380aa79993cd6713b6737024239d878d46d
prem.sasidharan@gmail.com <prem.sasidharan@gmail.com@7add919e-d01b-36b3-b92c-1f8e5742f5db>
2012-09-19 05:07:29 +0000

    code refactoring

--- trunk/apps/yuv_player/include/yuv_player.h
+++ trunk/apps/yuv_player/include/yuv_player.h
@@ -44,7 +44,7 @@ private:
     Yuv_player* player;
 };
 
-class Yuv_player:public QMainWindow, public Ui_MainWindow, public Observer
+class Yuv_player:public QMainWindow, public Ui_yuv_player, public Observer
 {
     Q_OBJECT
 public:
@@ -66,7 +66,9 @@ public:
     int set_parameters(int width, int height, Media::type fmt, float fps, const char* path);
     
 protected:
-    void initialize();
+    void init();
+	void init_player();
+	void init_actions();
     void connect_signals_slots();
     int event_handler(Media::events event, Abstract_media_object* obj, Media_params& params);
 
@@ -74,31 +76,13 @@ protected:
 
 protected slots:
     void time_out();
-
+	void file_open();
+	void help_about();
 	void change_screen_size();
 	void show_hide_progress_bar();
-
-	void file_open();
-
-	void mode_luma();
-	void mode_chromau();
-	void mode_chromav();
-
-	void mode_red();
-	void mode_green();
-	void mode_blue();
-
-	void mode_nyuv();
-	void mode_nrgb();
-	void mode_normal();
-
-	void text_mode_none();
-	void text_mode_time_code();
-	void text_mode_frame_count();
-
-	void help_about();
-
 	void playback_control(int status);
+	void change_disp_mode(QAction* action);
+	void change_text_mode(QAction* action);
 	void slider_seek(uint64_t _start, uint64_t _end);
 
 private:
@@ -116,6 +100,9 @@ private:
     
     Text_mode text_mode;
     Text_helper text_helper;
+
+	QActionGroup* mode_grp;
+	QActionGroup* text_grp;
 };
 
 #endif
